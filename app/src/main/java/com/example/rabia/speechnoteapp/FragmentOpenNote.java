1fe78749120022885e5ac9967d4d8b2c77587e6e
rabiayeler <yelerrabia@gmail.com>
2018-12-10 22:38:51 +0300

    İlk Repost

---
+++ app/src/main/java/com/example/rabia/speechnoteapp/FragmentOpenNote.java
@@ -0,0 +1,29 @@
+package com.example.rabia.speechnoteapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class FragmentOpenNote extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       // return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_open_note,container,false);
+
+
+    }
+
+
+}
